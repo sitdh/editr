018d498bf788ee4de929c71136b119b9d3fde10e
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-05-06 12:44:00 +0700

    layout api

--- app/Http/Controllers/API/EditorServicesController.php
+++ app/Http/Controllers/API/EditorServicesController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 
-class ExampleController extends APIController {
+class EditorServicesController extends Controller 
+{
+
+	public function __construct(){}
 
 	public function course($question_id) {
-		return response()->json($question_id);
+		return response()->json(['value' => $question_id]);
+	}
+
+	public function submit(Request $request)
+	{
+		// Get  test case
+		//
+
+		// Submit file and test case to compiler
+		//
+
+		// return result
 	}
 }
