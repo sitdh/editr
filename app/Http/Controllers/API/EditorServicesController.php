3ad5eca7577863fc44750f9515006ca7315f30cf
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-05-06 17:28:31 +0700

    Editor services

--- app/Http/Controllers/API/EditorServicesController.php
+++ app/Http/Controllers/API/EditorServicesController.php
@@ -19,15 +19,36 @@ class EditorServicesController extends Controller
 		return response()->json(['value' => $question_id]);
 	}
 
-	public function submit(Request $request, $id)
+	public function compile(Request $request)
 	{
-		// Get  test case
-		$question_info = $this->getTestcaseInformation($id);
-
 		// Submit file and test case to compiler
 		//
+		$script = $request->input('script'); 
+		$result = $this->getCompileResult($script);
 
 		// return result
+		return response()->json([
+			'compiled' => [
+				'results'	=> $result
+			]
+		]);
+	}
+
+	public function testScript(Request $request, $aid, $qid) 
+	{
+		// Get  test case
+		$testsuite = $this->getTestcaseInformation($aid, $qid);
+
+		$script = $request->input('script'); 
+		$result = $this->getTestResult($script, $testsuite);
+
+		// return result
+		return response()->json([
+			'tested' => [
+				'results'	=> $result
+			]
+		]);
+
 	}
 
 	protected function getContent($url)
@@ -41,11 +62,14 @@ class EditorServicesController extends Controller
 		return file_get_contents($url, false, $context);
 	}
 
-	protected function getTestcaseInformation($id) 
+	protected function getTestcaseInformation($aid, $qid) 
 	{
 		$content_url = sprintf(
 			config('service.course.testcase'),
-			$id	
+			1,
+			1
+			# $qid	
+			# $qid	
 		);
 		$raw_data = $this->getContent($content_url);
 
@@ -74,4 +98,56 @@ class EditorServicesController extends Controller
 		return $testcase;
 	}
 
+	protected function getCompileResult($script, $input = '')
+	{
+		$compileResult = $this->sendMessageToCompile(
+			config('service.compile.test'),
+			[
+				'lang'			=> 'python',
+				'sourceCode'	=> $script,
+				'input'			=> $input
+			]
+		);
+
+		return $compileResult;
+	}
+
+	protected function getTestResult($script, $testsuite)
+	{
+		$respond = [];
+		
+		foreach($testsuite as $testcase)
+		{
+			$output = array_pop($testcase);
+			$input = implode(' ', $testcase);
+			$compileResult = $this->sendMessageToCompile(
+				config('service.compile.test'),
+				[
+					'lang'			=> 'python',
+					'sourceCode'	=> $script,
+					'input'			=> $input,
+					'output'		=> $output
+				]
+			);
+
+			$respond[] = $compileResult;
+
+		}
+
+		return $respond;
+	}
+
+	protected function sendMessageToCompile($endpoint, $params)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $endpoint);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		$server_respond = curl_exec($ch);
+		curl_close($ch);
+
+		return $server_respond;
+	}
 }
