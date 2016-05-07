f74d6768d81246cc029241867160deea091f4420
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-05-07 16:53:14 +0700

    Update routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,8 +15,8 @@ $app->get('/', function () use ($app) {
     return response()->json(['a'=>'b']); // $app->version();
 });
 
-$app->get('api/question/{aid}-{qid}', 'API\EditorServicesController@question');
+$app->get('api/question', 'API\EditorServicesController@question');
 $app->post('api/question/compile', 'API\EditorServicesController@compile');
-$app->post('api/question/test/{aid}-{qid}', 'API\EditorServicesController@testScript');
+$app->post('api/question/test', 'API\EditorServicesController@testScript');
 
 # $app->get('api/question/{course_id}', 'API\EditorServicesController@course');
